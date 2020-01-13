ce7d5175fe592674592ae5de9f0d0a3a9be6de5f
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-01-13 09:32:55 -0500

    Initial maven project files

---
+++ leaderboard/src/main/java/com/diamondkinetics/codingchallenge/leaderboard/App.java
@@ -0,0 +1,13 @@
+package com.diamondkinetics.codingchallenge.leaderboard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
